da416f954f5e02adde47b56efee987c5090a87e2
HazzaCheng <hazzacheng@gmail.com>
2017-11-28 21:02:27 +0800

    add Maximum Subarray Problem

--- README.md
+++ README.md
@@ -1,12 +1,13 @@
 # Alogrithms
 Record some algorithms and data structures
 
-- `com.hazza.algorithms.rodCutting`:  Rod Cutting problem (solved by dynamic programming)
-- `com.hazza.algorithms.matrixChainMultiplication`: Matrix-chain Multiplication problem (solved by dynamic programming)
-- `com.hazza.algorithms.longestCommonSubsequence`: Longest-common-subsequence problem (solved by dynamic programming)
-- `com.hazza.algorithms.optimalBinarySearchTree`: Optimal Binary Search Tree(Huffman Tree) problem (solved by dynamic programming)
+- `com.hazza.algorithms.rodCutting`:  Rod Cutting problem (solved by DP)
+- `com.hazza.algorithms.matrixChainMultiplication`: Matrix-chain Multiplication problem (solved by DP)
+- `com.hazza.algorithms.longestCommonSubsequence`: Longest-common-subsequence problem (solved by DP)
+- `com.hazza.algorithms.optimalBinarySearchTree`: Optimal Binary Search Tree(Huffman Tree) problem (solved by DP)
 - `com.hazza.algorithms.activitySelector`: Activity-selection problem (solved by greedy)
 - `com.hazza.algorithms.huffmanCode`: Huffman Code problem (solved by greedy)
 - `com.hazza.algorithms.sumOfRange`: Sum Of A Range Of Elements problem (solved by segment tree)
 - `com.hazza.algorithms.rangeMinimumQuery`: Range Minimum Query problem (solved by segment tree and sparse table)
 - `com.hazza.algorithms.morrisTraversal`: Morris Traversal Problem
+- `com.hazza.algorithms.maximumSubarray`: Maximum Subarray Problem(solved by divide-and-conquer and DP)
---
+++ src/main/java/com/hazza/algorithms/maximumSubarray/MaximumSubarray.java
@@ -0,0 +1,92 @@
+package com.hazza.algorithms.maximumSubarray;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description: Solve Maximum Problem by divide-and-conquer and DP
+ * User: HazzaCheng
+ * Contact: hazzacheng@gmail.com
+ * Date: 17-11-28
+ * Time: 8:17 pm
+ */
+public class MaximumSubarray {
+    // solution 1
+
+    /**
+     * Find the maximum sub array.
+     * @param A The input array.
+     * @param low The start index of the sub array.
+     * @param high The end index of the sub array.
+     * @return An int array includes the start index of the maximum sub array,
+     *         the end index of the maximum sub array and the sum of maximum sub array.
+     */
+    int[] findMaximumSubarray(int[] A, int low, int high) {
+        if (low == high) return new int[]{low, high, A[low]};
+
+        int mid = (low + high) / 2;
+        int[] left = findMaximumSubarray(A, low, mid);
+        int[] right = findMaximumSubarray(A, mid + 1, high);
+        int[] cross = findMaxCrossingSubarray(A, low, mid, high);
+
+        if (left[2] >= right[2] && left[2] >= cross[2]) return left;
+        else if (right[2] >= left[2] && right[2] >= cross[2]) return right;
+        else return cross;
+    }
+
+    /**
+     * Find the maximum crossing sub array.
+     * @param A The input array.
+     * @param low The start index of the sub array.
+     * @param mid The mid index of the sub array.
+     * @param high The end index of the sub array.
+     * @return An int array includes the start index of the maximum sub array,
+     *         the end index of the maximum sub array and the sum of maximum sub array.
+     */
+    int[] findMaxCrossingSubarray(int[] A, int low, int mid, int high) {
+        int leftSum = Integer.MIN_VALUE, maxLeft = mid;
+        for (int i = mid, sum = 0; i >= low ; --i) {
+            sum += A[i];
+            if (sum > leftSum) {
+                leftSum = sum;
+                maxLeft = i;
+            }
+        }
+        int rightSum = Integer.MIN_VALUE, maxRight = mid + 1;
+        for (int i = mid + 1, sum = 0; i <= high; ++i) {
+            sum += A[i];
+            if (sum > rightSum) {
+                rightSum = sum;
+                maxRight = i;
+            }
+        }
+
+        return new int[]{maxLeft, maxRight, leftSum + rightSum};
+    }
+
+    // solution 2
+
+    /**
+     * Find the maximum sub array from the input array A.
+     * @param A the input array
+     * @return An int array includes the start index of the maximum sub array,
+     *         the end index of the maximum sub array and the sum of maximum sub array.
+     */
+    int[] findMaximumSubarrayDP(int[] A) {
+        int len = A.length;
+        int cur_left = 0, maxLeft = 0, maxRight = 0, temp = A[0], sumMax = A[0];
+
+        for (int i = 1; i < len; ++i) {
+            temp += A[i];
+            if (temp < 0) {
+                temp = 0;
+                cur_left = i + 1;
+            }
+            if (temp > sumMax) {
+                sumMax = temp;
+                maxLeft = cur_left;
+                maxRight = i;
+            }
+        }
+
+        return new int[]{maxLeft, maxRight, sumMax};
+    }
+}
