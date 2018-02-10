b933cbfc2512d9113b8ec1adb13083aeac152775
HazzaCheng <hazzacheng@gmail.com>
2018-02-10 21:54:23 +0800

    add Kruskal's Algorithm

---
+++ src/test/java/com/hazza/algorithms/kruskal/CreateMSTTest.java
@@ -0,0 +1,34 @@
+package com.hazza.algorithms.kruskal;
+
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: HazzaCheng
+ * Contact: hazzacheng@gmail.com
+ * Date: 18-2-10
+ * Time: 9:39 PM
+ */
+public class CreateMSTTest {
+
+    @Test
+    public void testExisted() {
+        Edge[] edges = new Edge[3];
+        edges[0] = new Edge(1, 2, 1);
+        edges[1] = new Edge(1, 3, 2);
+        edges[2] = new Edge(2, 3, 4);
+
+        assertEquals(3, CreateMST.kruskal(3, edges));
+    }
+
+    @Test (expected = RuntimeException.class)
+    public void nonExisted() {
+        Edge[] edges = new Edge[1];
+        edges[0] = new Edge(2, 3, 2);
+
+        CreateMST.kruskal(3, edges);
+    }
+}
